481b4410a68eba11f806a83ca66881eacbe98e88
andreea <andreea.mutu3@gmail.com>
2018-07-30 20:08:05 +0300

    fix rotation on news

--- app/src/main/java/com/andreea/cryptoright/ui/NewsViewModel.java
+++ app/src/main/java/com/andreea/cryptoright/ui/NewsViewModel.java
@@ -23,44 +23,47 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class NewsViewModel extends AndroidViewModel {
     private static final String TAG = NewsViewModel.class.getSimpleName();
+    private final CryptoCompareService service;
+    private LiveData<List<NewsArticle>> articles;
 
     public NewsViewModel(@NonNull Application application) {
         super(application);
-    }
-
-
-    public LiveData<List<NewsArticle>> getArticles() {
-
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Constants.COIN_API_BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        CryptoCompareService service = retrofit.create(CryptoCompareService.class);
-        Call<NewsResponse> coinPriceResponseCall = service.getNews(Constants.NEWS_LANGUAGE);
-        MutableLiveData<List<NewsArticle>> newsLiveData = new MutableLiveData<>();
-        coinPriceResponseCall.enqueue(new Callback<NewsResponse>() {
-            @Override
-            public void onResponse(@NonNull Call<NewsResponse> call, @NonNull Response<NewsResponse> response) {
-                if (response.isSuccessful()) {
-                    NewsResponse body = response.body();
-                    Log.d(TAG, "onResponse news resp: "+body);
-                    List<NewsArticle> articles = null;
-                    if (body != null) {
-                        articles = body.getData();
-                        Log.d(TAG, "onResponse news: "+ articles);
-                        newsLiveData.postValue(articles);
+        service = retrofit.create(CryptoCompareService.class);
+    }
+
+    public LiveData<List<NewsArticle>> getArticles() {
+
+        if (articles == null) {
+            Call<NewsResponse> coinPriceResponseCall = service.getNews(Constants.NEWS_LANGUAGE);
+            MutableLiveData<List<NewsArticle>> newsLiveData = new MutableLiveData<>();
+            coinPriceResponseCall.enqueue(new Callback<NewsResponse>() {
+                @Override
+                public void onResponse(@NonNull Call<NewsResponse> call, @NonNull Response<NewsResponse> response) {
+                    if (response.isSuccessful()) {
+                        NewsResponse body = response.body();
+                        Log.d(TAG, "Call to news service retrieved: " + body);
+                        List<NewsArticle> articles = null;
+                        if (body != null) {
+                            articles = body.getData();
+                            newsLiveData.postValue(articles);
+                        }
                     }
                 }
-            }
 
-            @Override
-            public void onFailure(@NonNull Call<NewsResponse> call, @NonNull Throwable t) {
-                Log.e(TAG, "Call to news service failed", t);
-                newsLiveData.postValue(Collections.emptyList());
-            }
-        });
+                @Override
+                public void onFailure(@NonNull Call<NewsResponse> call, @NonNull Throwable t) {
+                    Log.e(TAG, "Call to news service failed", t);
+                    newsLiveData.postValue(Collections.emptyList());
+                }
+            });
 
-        return newsLiveData;
+            articles = newsLiveData;
+        }
+        return articles;
     }
 }
